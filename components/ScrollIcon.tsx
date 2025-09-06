8a917af52004741a66b5241d29d80dd4ccd67cb7
Rafael Gaid Jr <rgaidjr@gmail.com>
2025-09-06 08:06:02 +0800

    smaller scroll icon

--- components/ScrollIcon.tsx
+++ components/ScrollIcon.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image';
 
 export default function ScrollIcon() {
   return (
-    <div className="fixed bottom-6 right-6 z-45 p-3">
+    <div className="fixed bottom-6 right-20 z-45 p-3">
       <Image
         src="/scroll-icon.webp"
         alt="Scroll icon"
         width={24}
         height={24}
-        className="w-24 h-24 object-contain"
+        className="w-16 h-16 object-contain"
       />
     </div>
   );
