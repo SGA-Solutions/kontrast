9c84477968ebe9db69357fa6826bc68274c13c82
Rafael Gaid Jr <rgaidjr@gmail.com>
2025-10-07 21:22:18 +0800

    added custom breakpoint 3xl

--- components/LayoutContent.tsx
+++ components/LayoutContent.tsx
@@ -13,10 +13,10 @@ export function LayoutContent({ children }: LayoutContentProps) {
   
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex-1" />
+      <div className="flex-1 3xl:min-h-[5vh]" />
       {/* Dynamic layout based on actual device type, not screen size */}
       <div className={`grid ${isMobile ? 'grid-cols-1' : 'grid-cols-[100px_1fr]'} grid-fallback`}>
-        {/* Left vertical navigation */}        
+        {/* Left vertical navigation */}      
         <Header />       
         <main className="max-h-[80vh] overflow-x-hidden overflow-y-auto hide-scrollbar mt-8 no-overscroll">
           {children}
